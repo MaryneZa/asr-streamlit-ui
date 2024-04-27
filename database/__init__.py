c9a52528bd3e834aa5600bdd03a5f51d34e2f268
MaryneZa <maryne6916@gmail.com>
2024-04-27 16:51:18 +0700

    pre data

--- database/__init__.py
+++ database/__init__.py
@@ -1,109 +1,211 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, storage
+import datetime
+
+# from firebase_admin import credentials
+
 import re
+import pandas as pd
 import os
 import json
 
+from uuid import uuid4
+
+# from google.cloud import storage
 
 
 # serviceAccount = os.path.join(os.path.dirname(__file__), ".", "serviceAccount.json")
+
+storageBucket = "asr-streamlit-6da8e.appspot.com"
 
 # Initialize Firebase Admin SDK
 # Check if the Firebase Admin SDK is already initialized
 if not firebase_admin._apps:
     # Initialize Firebase Admin SDK
-    cred = credentials.Certificate({
-        "type" : st.secrets["type"],
-        "project_id" : st.secrets["project_id"],
-        "private_key_id" : st.secrets["private_key_id"],
-        "private_key" : st.secrets["private_key"],
-        "client_email" : st.secrets["client_email"],
-        "client_id" : st.secrets["client_id"],
-        "auth_uri" : st.secrets["auth_uri"],
-        "token_uri" : st.secrets["token_uri"],
-        "auth_provider_x509_cert_url" : st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url" : st.secrets["client_x509_cert_url"],
-        "universe_domain" : st.secrets["universe_domain"]
-        })
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': 'asr-streamlit-6da8e.appspot.com'
-    })
+    cred = credentials.Certificate(
+        {
+            "type": st.secrets["type"],
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"],
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+            "universe_domain": st.secrets["universe_domain"],
+        }
+    )
+    firebase_admin.initialize_app(cred, {"storageBucket": storageBucket})
 
 # Get a reference to the Firebase Storage bucket
 bucket = storage.bucket()
 
+def upload_edited_csv_file(csv_content, csv_file_path):
 
-def get_last_string(source_path):
-    # Define the regular expression pattern to match the last string after the last "/"
-    pattern = r'[^/]+$'
-    
-    # Use re.search to find the matching substring
-    match = re.search(pattern, source_path)
-    
-    if match:
-        # Return the matching substring
-        return match.group(0)
-    else:
-        # If no match is found (no "/" in the path), return the original string
-        return source_path
-      
-
-def upload_csv_file(csv_content, csv_file_path):
-    path = get_last_string(csv_file_path)
-    print(csv_file_path)
+    bucket_path = f"csv_files/{csv_file_path}"
     # Check if the file already exists in Firebase Storage
-    existing_blob = bucket.blob(f"csv_files/{path}")
+    existing_blob = bucket.blob(bucket_path)
 
     # If the file exists, delete it
     if existing_blob.exists():
         existing_blob.delete()
-    
+
     # Convert the CSV content to bytes
     csv_content_bytes = csv_content.encode()
     # Upload the CSV content to Firebase Storage
-    blob = bucket.blob(f"csv_files/{path}")
-    blob.upload_from_string(csv_content_bytes, content_type='text/csv')
-  
-def upload_audio_file(audio_file_path):
-    path = get_last_string(audio_file_path)
-    # Upload the CSV file to Firebase Storage, overwriting the original file
-    blob = bucket.blob(f"audio_files/{path}")  # Path to the CSV file in Firebase Storage
-    blob.upload_from_filename(audio_file_path)
-    print("Audio file uploaded successfully.")
+    blob = bucket.blob(bucket_path)
+    blob.upload_from_string(csv_content_bytes, content_type="text/csv")
+    print("save edited file!")
 
-def list_files_in_folder(folder_path):
+
+def upload_csv_files(folder_path):
+    # Get the parent folder name
+    parent_folder_name = os.path.basename(folder_path)
+
+    # Iterate over the files in the folder
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".csv"):
+            # Read the CSV file into a DataFrame
+            file_path = os.path.join(folder_path, file_name)
+            df = pd.read_csv(file_path)
+            # Add a new column to the DataFrame
+            df["audio_link"] = get_audio_link(df, "full_path", parent_folder_name)
+            df["raw_text"] = df["text"]  # Example data for the new column
+
+            # Convert the DataFrame back to a CSV string
+            modified_csv_content = df.to_csv(index=False)
+            csv_content_bytes = modified_csv_content.encode()
+
+            # Define the folder name for Firebase Storage
+            folder_name = f"{parent_folder_name}"
+
+            # Upload the modified CSV content to Firebase Storage
+            blob = bucket.blob(f"csv_files/{folder_name}/{file_name}")
+            blob.upload_from_string(csv_content_bytes, content_type="text/csv")
+
+
+def name_csv_list(folder_path):
     # List all files in the specified folder
-    blobs = bucket.list_blobs(prefix=f"{folder_path}/")
+    blobs = bucket.list_blobs(prefix=f"{folder_path}")
 
-    # Extract file names from the blobs
-    file_names = [blob.name for blob in blobs]
+    folder_names = set()
+    for blob in blobs:
+        # Get the path after the prefix
+        relative_path = os.path.relpath(blob.name, folder_path)
+        # Extract folder name from the relative path
+        folder_name = os.path.dirname(relative_path)
+        print(f"folder_name : {folder_name}")
+        # Add folder name to the set
 
-    return file_names
+        folder_names.add(folder_name)
+    if "" in folder_names:
+        folder_names.remove("")
 
-def get_file_in_folder(folder_path):
+    return folder_names
+
+
+def get_csv_file(folder_path, file):
     if folder_path is None:
         return None, "Folder path is None.", None
     # Get the blob (file) within the specified folder
     path = folder_path.split("/")
-    # print(path)
-    blob = bucket.get_blob(folder_path)
+    blob = bucket.get_blob(f"csv_files/{folder_path}/{file}")
 
     if blob is not None:
-        return blob.download_as_string(), None, folder_path  # Return the file content as bytes and no error message
+        return (
+            blob.download_as_string(),
+            None,
+        )  # Return the file content as bytes and no error message
     else:
-        return None, f"File '{path[1]}' not found in folder '{path[0]}'.", path
+        return None, f"File '{path[1]}' not found in folder '{path[0]}'."
 
-# def upload_csv_file(csv_file_path):
-#     path = get_last_string(csv_file_path)
-#     # Upload the CSV file to Firebase Storage, overwriting the original file
-#     blob = bucket.blob(f"csv_files/{path}")  # Path to the CSV file in Firebase Storage
-#     blob.upload_from_filename(csv_file_path)
-#     print("CSV file uploaded successfully.")
 
-# def download_csv_file(destination_file_path):
-#     # Download the CSV file from Firebase Storage
-#     blob = bucket.blob(f"csv_files/{destination_file_path}")  # Path to the CSV file in Firebase Storage
-#     blob.download_to_filename(f"D:/{destination_file_path}")
+def get_audio_link(df, columns, folder_name):
+    # print(df[columns])
+    audio_link = []
+    for index, row in df.iterrows():
 
-#     print("CSV file downloaded successfully.")
+        audio_path = row[columns].split("/")[-1]
+
+        bucket_path = f"audio_files/{folder_name}/{audio_path}"
+
+        # Get a blob reference to the audio file
+        blob = bucket.get_blob(bucket_path)
+
+        # Check if the blob exists
+        if not blob.exists():
+            audio_link.append(None)
+            continue
+
+        # Fetches object metadata
+        metadata = blob.metadata
+
+        # Firebase Access Token
+        token = metadata["firebaseStorageDownloadTokens"]
+
+        firebase_storageURL = "https://firebasestorage.googleapis.com/v0/b/{}/o/{}?alt=media&token={}".format(
+            storageBucket, bucket_path.replace("/", "%2F").replace(" ", "%20"), token
+        )
+
+        audio_link.append(firebase_storageURL)
+
+    print(f"len link: {len(audio_link)}")
+    print(f"len df: {len(df)}")
+    return audio_link
+
+
+def download_csv_file(destination_file_path):
+    # Download the CSV file from Firebase Storage
+    blob = bucket.blob(
+        f"csv_files/{destination_file_path}"
+    )  # Path to the CSV file in Firebase Storage
+    blob.download_to_filename(f"D:/{destination_file_path}")
+
+    print("CSV file downloaded successfully.")
+
+
+def upload_audio_files(folder_path):
+    # Get the parent folder name
+    parent_folder_name = os.path.basename(folder_path)
+
+    # Iterate over the files in the folder
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".wav"):
+            # Read the CSV file into a DataFrame
+            file_path = os.path.join(folder_path, file_name)
+
+            # Upload the modified CSV content to Firebase Storage
+            bucket_path = f"audio_files/{parent_folder_name}/{file_name}"
+            blob = bucket.blob(bucket_path)
+
+            token = uuid4()
+            metadata = {"firebaseStorageDownloadTokens": token}
+            # Assign the token as metadata
+            blob.metadata = metadata
+
+            blob.upload_from_filename(file_path)
+
+            # Make the file public (OPTIONAL). To be used for Cloud Storage URL.
+            blob.make_public()
+
+            # Fetches a public URL from GCS.
+            gcs_storageURL = blob.public_url
+
+            # Generates a URL with Access Token from Firebase.
+            firebase_storageURL = "https://firebasestorage.googleapis.com/v0/b/{}/o/{}?alt=media&token={}".format(
+                storageBucket,
+                bucket_path.replace("/", "%2F").replace(" ", "%20"),
+                token,
+            )
+
+            print(
+                {
+                    "gcs_storageURL": gcs_storageURL,
+                    "firebase_storageURL": firebase_storageURL,
+                }
+            )
+
+            print("Audio file uploaded successfully.")
