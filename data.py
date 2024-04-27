f2f7feb0ed5033ccc26c108784a685c72ac20434
MaryneZa <maryne6916@gmail.com>
2024-04-27 16:51:49 +0700

    ui

---
+++ data.py
@@ -0,0 +1,9 @@
+from database import upload_csv_files, upload_audio_files
+
+#plz upload your audio files before upload your csv files
+#your audio and csv folder name should be the same
+
+# upload_audio_files(r"D:\work\Natcha NCS-01\Natcha-01")
+
+upload_csv_files(r"D:\work\Natcha-01")
+
--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import streamlit as st
 import pandas as pd 
 import io
 import time
-from database import get_file_in_folder, list_files_in_folder, upload_audio_file, upload_csv_file, get_last_string
+from database import get_csv_file, name_csv_list, upload_edited_csv_file
+
+
+def convert_df():
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    df = st.session_state.dataframe
+    return df.to_csv().encode('utf-8')
 
 st.set_page_config(
     page_title="Editing CSV",
     page_icon="🧊",
     layout="centered",
-    initial_sidebar_state="expanded",
 )
 
 def load_data_for_page(data_frame, page_number, rows_per_page):
@@ -19,50 +24,68 @@ def load_data_for_page(data_frame, page_number, rows_per_page):
 # Function to save edited CSV data
 def save_edited_csv(data_frame, csv_path):
     edited_csv = data_frame.to_csv(index=False)
-    upload_csv_file(edited_csv, csv_path)
+    upload_edited_csv_file(edited_csv, csv_path)
     st.toast(":green[Edited CSV file saved successfully.]", icon="🎉")
 
-def handle_selected_csv_file():
+def handle_selected_file():
     st.session_state.clear()
-    st.toast("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+
+def handle_next_page():
+    save_edited_csv(
+        st.session_state.dataframe, 
+        f"{st.session_state.selected_csv_file}/{st.session_state.file}"
+        )
+    st.session_state.page_number += 1
+
+def handle_previous_page():
+    st.session_state.page_number -=1
+    
 
 
 def main():
-    st.sidebar.title(":blue[EDITING CSV] 🧊")
     
-    csv_list = list_files_in_folder("csv_files")
-
-    selected_csv_file = st.sidebar.selectbox(
-        ":blue[Select a CSV ...]", 
-        csv_list, 
-        index=None,
-        placeholder="please select ...",
-        on_change=handle_selected_csv_file
-        )
+    cols_head = st.columns([0.5,0.3,0.2], gap="small")
+    with cols_head[0]:
+        st.title(":blue[CSV TextEdit Hub]")
+    with cols_head[1]:
+        st.image("./assets/cat.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="png")
     
-            
+    
+    csv_list = name_csv_list("csv_files")
 
-    if selected_csv_file:
-        # # Check if selected_csv_file has changed
-        # if selected_csv_file in st.session_state:
-        #     if st.session_state.selected_csv_file != selected_csv_file:
-        #         st.session_state.clear()
-        #     st.session_state.selected_csv_file = selected_csv_file
-        #     csv, _, csv_path = get_file_in_folder(st.session_state.selected_csv_file)
+    cols_file = st.columns([0.6,0.4], gap="small")
+    
+    with cols_file[0]:
+        selected_csv_file = st.selectbox(
+            ":blue[Select Name]", 
+            csv_list, 
+            index=None,
+            placeholder="please select ...",
+            on_change=handle_selected_file
+            )
+    with cols_file[1]:
+        file = st.selectbox(
+            ":blue[Select File]", 
+            ["train.csv", "val.csv"],
+            index=None,
+            placeholder="please select ...",
+            on_change=handle_selected_file
+            )
+        
+        if file and 'file' not in st.session_state:
+            st.session_state.file = file
+        
+    if selected_csv_file and file:
         
         st.session_state.selected_csv_file = selected_csv_file
         
-        csv, _, csv_path = get_file_in_folder(st.session_state.selected_csv_file)
+        csv, message = get_csv_file(st.session_state.selected_csv_file,file)
         
         if csv:
             data = pd.read_csv(io.BytesIO(csv))
             df = pd.DataFrame(data)
             
-            selected_csv = st.sidebar.selectbox(
-                                ':blue[Select a Column ( Text )]', 
-                                data.columns, 
-                                index=None,
-                                placeholder="please select ...")
+            selected_csv = "text"
             
             if selected_csv:
                 if 'dataframe' not in st.session_state:
@@ -70,83 +93,63 @@ def main():
                 
                 if 'edited_row' not in st.session_state:
                     st.session_state.edited_row = []
+                
+                if 'page_number' not in st.session_state:
+                    st.session_state.page_number = 1
         
-                selected_audio = st.sidebar.selectbox(
-                    ":blue[Select a Column (Audio path)]", 
-                    data.columns, 
-                    index=None,
-                    placeholder="please select ...")
+                
+                
+                selected_audio = "full_path"
                 
                 if selected_audio:
-                    
-                    rows_per_page = 5
+                    rows_per_page = 10
                     total_rows = len(df)
                     total_pages = total_rows // rows_per_page + 1 if total_rows % rows_per_page != 0 else total_rows // rows_per_page
-                    tabs = st.radio(":blue[Pages]", ("ALL DATA", "Edited DATA (unsaved)"), horizontal=True)
-                    st.divider()
-                    if tabs == "ALL DATA":
-                        page_number = st.sidebar.number_input(":blue[Page Number]", min_value=1, max_value=total_pages, value=1)
-                        selected_csv_data = load_data_for_page(st.session_state.dataframe, page_number - 1, rows_per_page)
-                        # st.sidebar.write(selected_csv_data)
-                        for index, row in selected_csv_data[[selected_csv]].iterrows():
-                            st.write(f":blue[Index : {index}]")
-                            with st.container(border=True):
-                        
-                                audio_path = get_last_string(df[selected_audio][index])
-                                audio, _, _ = get_file_in_folder(f"audio_files/{audio_path}")
-                                if audio:
-                                    st.audio(audio, format="audio/wav")
-                                
-                                st.text_input(f"Default {selected_csv}", value=df[selected_csv][index], disabled=True, autocomplete="off")
-                                
-                                text_input_key = f"text-{index}-{selected_csv}"
-                                # Get the edited value from the user
-                                edited_value = st.text_input(f"Edit {selected_csv}", value=row[selected_csv], key=text_input_key, autocomplete="off")
-                                
-                                # st.button("Reset", type="primary")
-                                
-                                if edited_value != row[selected_csv]:
-                                    with st.spinner('Updating...'):
-                                        st.session_state.dataframe[selected_csv][index] = edited_value
-                                        if index not in st.session_state.edited_row:
-                                            st.session_state.edited_row.append(index)
-                                        st.success(' Edited Successfully!')
-                                        
-                            st.divider()
+                    selected_csv_data = load_data_for_page(st.session_state.dataframe, st.session_state.page_number - 1, rows_per_page)
+                    # st.sidebar.write(selected_csv_data)
+                    for index, row in selected_csv_data[[selected_csv]].iterrows():
+                        st.write(f":blue[Index : {index}]")
+                        with st.container(border=True):
+                    
+                            st.audio(df["audio_link"][index], format="audio/wav")
                             
-                    if tabs == "Edited DATA (unsaved)":
-                        st.write(f":blue[Edited Index : {' , '.join(map(str, st.session_state.edited_row))}]")
-                        st.divider()
-
-                        selected_rows = st.session_state.dataframe.loc[st.session_state.edited_row, [selected_csv]]
-                        for index, row in selected_rows.iterrows():
-                            st.write(f":blue[Index : {index}]")
-                            with st.container(border=True):
-                                                
-                                audio_path = get_last_string(df[selected_audio][index])
-                                audio, _, _ = get_file_in_folder(f"audio_files/{audio_path}")
-                                if audio:
-                                    st.audio(audio, format="audio/wav")
+                            st.text_input(f"Default {selected_csv}", value=df["raw_text"][index], disabled=True, autocomplete="off")
+                            
+                            text_input_key = f"text-{index}-{selected_csv}"
+                            # Get the edited value from the user
+                            edited_value = st.text_input(f"Edit {selected_csv}", value=row[selected_csv], key=text_input_key, autocomplete="off")
+                            
+                            if edited_value != row[selected_csv]:
+                                with st.spinner('Updating...'):
+                                    st.session_state.dataframe[selected_csv][index] = edited_value
+                                    if index not in st.session_state.edited_row:
+                                        st.session_state.edited_row.append(index)
+                                    st.success(' Edited Successfully!')
                                     
-                                st.text_input(f"Default {selected_csv}", value=df[selected_csv][index], disabled=True, autocomplete="off")
-                                
-                                text_input_key = f"edited-text-{index}-{selected_csv}"
-                                # Get the edited value from the user
-                                edited_value = st.text_input(f"Edit {selected_csv}", value=row[selected_csv], key=text_input_key, autocomplete="off")
-                                
-                                if edited_value != row[selected_csv]:
-                                    with st.spinner('Updating...'):
-                                        st.session_state.dataframe[selected_csv][index] = edited_value
-                                        st.success(' Edited Successfully!')
-                                        
-                            st.divider()
-
-                    save_edited_csv_btn = st.sidebar.button("Save edited CSV",type="primary")
-                    if save_edited_csv_btn:
-                        save_edited_csv(st.session_state.dataframe, csv_path)
-                        st.session_state.clear()
-                        st.switch_page("main.py")
-
+                        st.divider()
+                            
+                    cols = st.columns([0.2,0.6,0.2], gap="large")
+                    with cols[0]:
+                        if st.session_state.page_number > 1:
+                            st.button("Previous", type="primary", on_click=handle_previous_page)
+                            
+                    # with cols[1]:
+                    #     st.write(st.session_state.page_number)
+                        
+                    with cols[2]:
+                        if st.session_state.page_number <= total_pages:
+                            st.button("Next Page", type="primary", on_click=handle_next_page)
+                                # save_edited_csv(st.session_state.dataframe, f"{selected_csv_file}/{file}")
+                        else:
+                            st.download_button(
+                                label=":blue[Download CSV]",
+                                data=convert_df(),
+                                file_name=f'{selected_csv_file}_{file}.csv',
+                                mime='text/csv',
+                                # type="primary",
+                            )
+        else :
+            st.toast(message)                    
                             
 if __name__ == "__main__":
     main()
